276f1c2cedafc3ffc8372ca36e66ed6b78509017
johnny990628 <johnny99457@gmail.com>
2022-02-02 02:38:38 +0800

    feat:userQuery

--- models/user/userQuery.js
+++ models/user/userQuery.js
@@ -3,8 +3,19 @@ const query = require('../../mysql/connetion');
 module.exports = {
     register: async (user) => {
         try {
-            let sql = `insert into user(name,username,password) values ('${user.name}','${user.username}','${user.password}')`;
-            return await query(sql);
+            let isExistSQL = `select * from user where username = '${user.username}'`;
+            let insertSQL = `insert into user(name,username,password) values ('${user.name}','${user.username}','${user.password}')`;
+            const exists = await query(isExistSQL);
+            return !exists.length ? await query(insertSQL) : null;
+        } catch (e) {
+            return e;
+        }
+    },
+    login: async (user) => {
+        try {
+            let selectSQL = `select * from user where username = '${user.username}'`;
+            const result = await query(selectSQL);
+            return !result.length ? null : result[0];
         } catch (e) {
             return e;
         }
