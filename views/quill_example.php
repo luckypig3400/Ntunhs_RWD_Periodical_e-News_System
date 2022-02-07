5f6197f6b00067605db4b68fcb2611370e311dab
郵差 <twHenryYang@gmail.com>
2022-02-07 12:54:10 +0800

    Add more quill toolbar options

--- views/quill_example.php
+++ views/quill_example.php
@@ -25,9 +25,6 @@ require("./partials/head.php");
 
                 <!-- Create the editor container -->
                 <div id="editor">
-                    <p>Hello World!</p>
-                    <p>Some initial <strong>bold</strong> text</p>
-                    <p><br></p>
                 </div>
 
             </div>
@@ -38,9 +35,40 @@ require("./partials/head.php");
 
         <!-- Initialize Quill editor -->
         <script>
-            var quill = new Quill('#editor', {
-                theme: 'snow'
-            });
+            // https://quilljs.com/docs/modules/toolbar/
+            var toolbarOptions = [
+                ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+                ['blockquote', 'code-block'],
+                [{
+                    'header': [1, 2, 3, 4, 5, 6, false]
+                }],
+
+                [{
+                    'color': []
+                }, {
+                    'background': []
+                }], // dropdown with defaults from theme
+                [{
+                    'font': []
+                }],
+                [{
+                    'align': []
+                }],
+
+                ['clean'] // remove formatting button
+            ];
+
+            var quillOptions = {
+                // readOnly: true,
+                theme: 'snow',
+                modules: {
+                    toolbar: toolbarOptions
+                },
+                // https://stackoverflow.com/questions/39456273/how-do-i-create-a-quill-editor-without-a-toolbar
+                // border: none
+            }
+
+            var quill = new Quill('#editor', quillOptions);
         </script>
     </main>
 
