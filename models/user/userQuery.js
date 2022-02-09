60a7e692769207f6541623952799b798f0f2a13a
johnny99457 <johnny99457@gmail.com>
2022-02-09 15:29:32 +0800

    fix:check if username was used

--- models/user/userQuery.js
+++ models/user/userQuery.js
@@ -1,15 +1,15 @@
-const query = require('../../mysql/connetion');
+const query = require("../../mysql/connetion");
 
 module.exports = {
-    register: async (user) => {
-        let isExistSQL = `select * from user where username = '${user.username}'`;
-        let insertSQL = `insert into user(name,username,password) values ('${user.name}','${user.username}','${user.password}')`;
-        const exists = await query(isExistSQL);
-        return !exists.length ? await query(insertSQL) : null;
-    },
-    login: async (user) => {
-        let selectSQL = `select * from user where username = '${user.username}'`;
-        const result = await query(selectSQL);
-        return !result.length ? null : result[0];
-    },
+  register: async (user) => {
+    let isExistSQL = `select exists(select 1 from user where username = '${user.username}') as isExists`;
+    let insertSQL = `insert into user(name,username,password) values ('${user.name}','${user.username}','${user.password}')`;
+    const existsResult = await query(isExistSQL);
+    return existsResult[0].isExists ? await query(insertSQL) : null;
+  },
+  login: async (user) => {
+    let selectSQL = `select * from user where username = '${user.username}'`;
+    const result = await query(selectSQL);
+    return !result.length ? null : result[0];
+  },
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,70 +1,79 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const USER = require('../models/user/userQuery');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const USER = require("../models/user/userQuery");
 
 const verifyToken = (req, res, next) => {
-    const accessToken =
-        req.cookies.accessToken ||
-        (req.headers['authorization'] ? req.headers['authorization'].split(' ').pop() : null);
+  const accessToken =
+    req.cookies.accessToken ||
+    (req.headers["authorization"]
+      ? req.headers["authorization"].split(" ").pop()
+      : null);
 
-    if (accessToken) {
-        jwt.verify(accessToken, process.env.JWT_SECRECT_KEY, (err, token) => {
-            if (err) {
-                return res.status(403).json({ message: 'Invalid token' });
-            } else {
-                req.token = token;
-                next();
-            }
-        });
-    } else {
-        return res.status(403).json({ message: 'Invalid token' });
-    }
+  if (accessToken) {
+    jwt.verify(accessToken, process.env.JWT_SECRECT_KEY, (err, token) => {
+      if (err) {
+        return res.status(403).json({ message: "Invalid token" });
+      } else {
+        req.token = token;
+        next();
+      }
+    });
+  } else {
+    return res.status(403).json({ message: "Invalid token" });
+  }
 };
 
-router.route('/login').post(async (req, res) => {
-    try {
-        const { username, password, name } = req.body;
-        let user = await USER.login({ name, username, password });
+router.route("/login").post(async (req, res) => {
+  try {
+    const { username, password, name } = req.body;
+    let user = await USER.login({ name, username, password });
 
-        if (!user) return res.status(400).json({ message: 'user not found' });
+    if (!user) return res.status(400).json({ message: "user not found" });
 
-        if (await bcrypt.compare(password, user.password)) {
-            const accessToken = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRECT_KEY, {
-                expiresIn: 60 * 60 * 24,
-            });
-            delete user.password;
-            return res
-                .cookie('accessTokens', accessToken, {
-                    maxAge: 60 * 60 * 24,
-                    secure: false, //set true if using https
-                    httpOnly: true, //can't access from javascript
-                })
-                .status(200)
-                .json({ message: 'login successful', user, token: accessToken });
-        } else {
-            return res.status(400).json({ message: 'password incorrect' });
+    if (await bcrypt.compare(password, user.password)) {
+      const accessToken = jwt.sign(
+        { id: user.id, username: user.username },
+        process.env.JWT_SECRECT_KEY,
+        {
+          expiresIn: 60 * 60 * 24,
         }
-    } catch (e) {
-        return res.status(500).json({ error: e });
+      );
+      delete user.password;
+      return res
+        .cookie("accessTokens", accessToken, {
+          maxAge: 60 * 60 * 24,
+          secure: false, //set true if using https
+          httpOnly: true, //can't access from javascript
+        })
+        .status(200)
+        .json({ message: "login successful", user, token: accessToken });
+    } else {
+      return res.status(400).json({ message: "password incorrect" });
     }
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
 });
 
-router.route('/register').post(async (req, res) => {
-    try {
-        let { username, password, name } = req.body;
-        if (!name || !username || !password) return res.status(400).json({ message: 'missing required field' });
-        password = await bcrypt.hash(password, 10);
+router.route("/register").post(async (req, res) => {
+  try {
+    let { username, password, name } = req.body;
+    if (!name || !username || !password)
+      return res.status(400).json({ message: "missing required field" });
+    password = await bcrypt.hash(password, 10);
 
-        const exist = await USER.register({ name, username, password });
-        if (exist) return res.status(400).json({ message: 'username was used' });
+    const exist = await USER.register({ name, username, password });
+    if (exist) return res.status(400).json({ message: "username was used" });
 
-        return res.status(200).json({ message: 'register successful', name, username });
-    } catch (e) {
-        return res.status(500).json({ error: e });
-    }
+    return res
+      .status(200)
+      .json({ message: "register successful", name, username });
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
 });
 
 module.exports = { router, verifyToken };
