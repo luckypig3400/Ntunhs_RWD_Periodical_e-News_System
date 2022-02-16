5587fdbc590599dc954d7049842b72d39bb4c511
楊嘉翔 <81738019+Yang-Jiaxiang@users.noreply.github.com>
2022-02-16 20:35:58 +0800

    separate dataProvider&App.JS

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,13 @@
 import * as React from "react";
-import {fetchUtils, Admin, Resource ,ShowGuesser } from 'react-admin';
+import { Admin, Resource ,ShowGuesser } from 'react-admin';
 import { UserList,UserEdit,UserCreate } from './component/users';
 import { PostList, PostEdit, PostCreate } from './component/posts';
 import {CategorysList,CategorysEdit,CategorysCreate} from './component/categorys'
 import authProvider from './authProvider';
-import jsonServerProvider from 'ra-data-json-server';
-//import simpleRestProvider from 'ra-data-simple-rest';
+import dataProvider from './dataProvider'
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import CategoryIcon from '@material-ui/icons/Category';
-
-// http://localhost:5000
-// https://jsonplaceholder.typicode.com
-const dataProvider = jsonServerProvider('http://localhost:5000');
-
-/*
-const fetchJson = (url, options = {}) => {
-  if (!options.headers) {
-      options.headers = new Headers({ Accept: 'application/json' });
-  }
-  // add your own headers here
-  options.headers.set('X-Custom-Header', 'foobar');
-  return fetchUtils.fetchJson(url, options);
-}
-//const dataProvider = simpleRestProvider('http://localhost:5000', fetchJson);
-*/
 
   
 const App = () => (
---
+++ client/src/dataProvider.js
@@ -0,0 +1,22 @@
+
+import jsonServerProvider from 'ra-data-json-server';
+//import simpleRestProvider from 'ra-data-simple-rest';
+
+
+// http://localhost:5000
+// https://jsonplaceholder.typicode.com
+const dataProvider = jsonServerProvider('http://localhost:5000');
+
+export default dataProvider;
+
+/*
+const fetchJson = (url, options = {}) => {
+  if (!options.headers) {
+      options.headers = new Headers({ Accept: 'application/json' });
+  }
+  // add your own headers here
+  options.headers.set('X-Custom-Header', 'foobar');
+  return fetchUtils.fetchJson(url, options);
+}
+//const dataProvider = simpleRestProvider('http://localhost:5000', fetchJson);
+*/
