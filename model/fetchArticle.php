233da45548a97b0254c9ba8a593d5ec45ea5cf13
郵差 <twHenryYang@gmail.com>
2022-02-21 14:53:00 +0800

    Update fetchArticle.php

--- model/fetchArticle.php
+++ model/fetchArticle.php
@@ -31,14 +31,17 @@ function fetchArticleList($in_period, $in_category)
 {
     $period = str_replace('/[^A-Za-z0-9\-]/', '', $in_period); // Removes all special chars.
     $category = str_replace('/[^A-Za-z0-9\-]/', '', $in_category); // Removes all special chars.
+    $getAllCategories = isset($_GET['getAllCategories']) ? $_GET['getAllCategories'] : "";
 
     if ($period != "" && $category != "") {
         $sql = "SELECT * FROM periodical WHERE periodNumber = '$period' AND categoryID = '$category'";
     } else if ($period != "") {
         $sql = "SELECT * FROM periodical WHERE periodNumber = '$period'";
-    } else if ($category != "") {
+    } else if ($category != "" && $getAllCategories == "true") {
         // 同一分類的文章太多，因此只提供該分類全部文章的摘要
         $sql = "SELECT id,subject,photo FROM periodical WHERE categoryID = '$category'";
+    } else if ($category != "") {
+        $sql = "SET @newestPeriod=(SELECT periodNumber FROM periodical ORDER BY updateTime DESC LIMIT 1); SELECT * FROM periodical WHERE categoryID = '$category' AND periodNumber = @newestPeriod;";
     } else {
         // default select the latest period articles
         // https://stackoverflow.com/questions/11754781/how-to-declare-a-variable-in-mysql
@@ -55,9 +58,13 @@ function fetchArticleList($in_period, $in_category)
         $stmt->setFetchMode(PDO::FETCH_ASSOC); // set the resulting array to associative
 
         $result = $stmt->fetchAll();
-
-        return $result;
     } catch (PDOException $e) {
         return "Error Occured while Fetching Article List:" . $e->getMessage();
+    }
+
+    if (sizeof($result) == 0) {
+        return "很抱歉`(*>﹏<*)′本期該分類尚未有文章(っ °Д °;)っ<br>Sorry＞﹏＜! Current period doesn't have any atrticle for this category. OAO";
+    } else {
+        return $result;
     }
 }
