ae12befb23fe8bcb790756484bde80ff653995bf
楊嘉翔 <81738019+Yang-Jiaxiang@users.noreply.github.com>
2022-03-30 22:01:54 +0800

    修改使用者 刪除使用者

--- src/page/User.js
+++ src/page/User.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import axios from "axios";
-import {
-    Button,
-} from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 const config = require("../config/default.json");
 
@@ -11,6 +9,9 @@ const apiURL = config.apiURL;
 
 const User = () => {
     const [category, setCategory] = useState([]);
+    const [createUserButton, setUserButton] = useState(true);
+    const [userEditRender, setUserEditRender] = useState(false);
+    const [userContent, setUserContent] = useState({});
     useEffect(() => {
         axios
             .get(`${apiURL}/api/user`)
@@ -35,8 +36,121 @@ const User = () => {
         },
     ];
 
+    const EditUser = () => {
+        axios.defaults.withCredentials = true;
+        axios
+            .patch(`${apiURL}/api/user/${userContent.ID}`, {
+                name: userContent.name,
+                username: userContent.username,
+                password: userContent.password,
+            })
+            .then((result) => {
+                setCategory(result.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    const DeleteUser = () => {
+        axios.defaults.withCredentials = true;
+        axios
+            .delete(`${apiURL}/api/user/${userContent.ID}`)
+            .then((result) => {
+                setCategory(result.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    const userEditDiv = () => {
+        return (
+            <div style={{ marginTop: "20px" }}>
+                姓名：
+                <TextField
+                    id="outlined-basic"
+                    value={userContent.name}
+                    variant="outlined"
+                    onChange={(event) => {
+                        setUserContent({
+                            ...userContent,
+                            name: event.target.value,
+                        });
+                    }}
+                    sx={{ marginRight: "20px" }}
+                />
+                帳號：
+                <TextField
+                    id="outlined-basic"
+                    value={userContent.username}
+                    variant="outlined"
+                    onChange={(event) => {
+                        setUserContent({
+                            ...userContent,
+                            username: event.target.value,
+                        });
+                    }}
+                    sx={{ marginRight: "20px" }}
+                />
+                新密碼：
+                <TextField
+                    id="outlined-basic"
+                    value={userContent.password}
+                    variant="outlined"
+                    onChange={(event) => {
+                        setUserContent({
+                            ...userContent,
+                            password: event.target.value,
+                        });
+                        console.log(userContent);
+                    }}
+                    sx={{ marginRight: "20px" }}
+                />
+                <div style={{ marginTop: "20px" }}>
+                    <Button
+                        variant="contained"
+                        sx={{
+                            height: "55px",
+                            width: "100px",
+                            marginRight: "20px",
+                        }}
+                        onClick={() => {
+                            EditUser();
+                            setUserEditRender(false);
+                            window.location.reload();
+                        }}
+                    >
+                        修改
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        color="error"
+                        sx={{ height: "55px", width: "auto" }}
+                        onClick={() => {
+                            var message = window.confirm(
+                                `確定要刪除${userContent.name}?`
+                            );
+                            if (message === true) {
+                                alert("OK");
+                                DeleteUser();
+                                setUserEditRender(false);
+                                window.location.reload();
+                            } else {
+                                alert("取消刪除");
+                                window.location.reload();
+                            }
+                        }}
+                    >
+                        刪除使用者
+                    </Button>
+                </div>
+            </div>
+        );
+    };
+
     return (
-        <>  
+        <>
             <div className="headerTitle">成員管理</div>
             <div className="pagecontent">
                 <div style={{ display: "flex", height: "100%" }}>
@@ -45,11 +159,27 @@ const User = () => {
                             rows={category}
                             columns={columns}
                             autoHeight={true}
+                            onRowClick={(user) => {
+                                setUserContent({
+                                    ID: user.row.id,
+                                    username: user.row.username,
+                                    name: user.row.name,
+                                });
+                                setUserEditRender(true);
+                                setUserButton(false);
+                            }}
                         />
                     </div>
                 </div>
-                <div style={{paddingTop:"20px"}}></div>
-                <Link to="/CreateUser"><Button variant="contained" >註冊新使用者</Button></Link>
+                <div style={{ paddingTop: "20px" }}></div>
+                {createUserButton ? (
+                    <Link to="/CreateUser">
+                        <Button variant="contained">註冊新使用者</Button>
+                    </Link>
+                ) : (
+                    ""
+                )}
+                {userEditRender ? userEditDiv() : ""}
             </div>
         </>
     );
