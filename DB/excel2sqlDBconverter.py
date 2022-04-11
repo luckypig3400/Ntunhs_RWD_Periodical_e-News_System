e7ccb9430f138f26705924f6bdca3e9a3e13c4ff
郵差 <twHenryYang@gmail.com>
2022-04-11 20:50:43 +0800

    feat: auto create table if not exists

--- DB/excel2sqlDBconverter.py
+++ DB/excel2sqlDBconverter.py
@@ -22,7 +22,8 @@ try:
     if(userInput == "y"):
         print(userInput)
         cursor = conn.cursor()
-        cursor.execute("CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'utf8'".format(config["db"]))
+        cursor.execute(
+            "CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'utf8'".format(config["db"]))
         print("Database '{}' established".format(config["db"]))
         cursor.close()
         conn.close()
@@ -42,37 +43,42 @@ else:
     # 獲取表單
     sheet = workbook['Content']
     # 讀取指定的單元格資料
-    for j in range(2, 94):
-        for i in range(1, 95):
+    for j in range(1, sheet.max_row + 1):
+        for i in range(1, sheet.max_column + 1):
             cell = sheet.cell(row=j, column=i).value
-            # if j == 1:
-            #     print(str(i) + ":" + str(cell))
-            #     # 列印欄位標題
-            if i == 2:
-                MEETING_TITLE = cell
-                print('存檔MEETING_TITLE')
+            createTableSQLcommand = "CREATE TABLE IF NOT EXISTS `content` (`Serial` int(11) NOT NULL,`NoID` varchar(50) DEFAULT NULL,`NoYear` varchar(4) DEFAULT NULL,`NoMonth` varchar(2) DEFAULT NULL,`NoClass` varchar(50) DEFAULT NULL,`Subject` varchar(50) DEFAULT NULL,`Writer` varchar(50) DEFAULT NULL,`StarID` int(11) DEFAULT NULL,`Email` varchar(50) DEFAULT NULL,`Content1` mediumtext DEFAULT NULL,`Content2` mediumtext DEFAULT NULL,`Content3` mediumtext DEFAULT NULL,`Photo1` varchar(50) DEFAULT NULL,`Photo2` varchar(50) DEFAULT NULL,`Photo3` varchar(50) DEFAULT NULL,`Alt1` varchar(50) DEFAULT NULL,`Alt2` varchar(50) DEFAULT NULL,`Alt3` varchar(50) DEFAULT NULL,`Temp1` varchar(15) DEFAULT NULL,`Temp2` varchar(15) DEFAULT NULL,`Temp3` varchar(15) DEFAULT NULL,`TempColor` varchar(50) DEFAULT NULL,`Link1` mediumtext DEFAULT NULL,`Link2` mediumtext DEFAULT NULL,`Link3` mediumtext DEFAULT NULL,`Linkloc1` varchar(50) DEFAULT NULL,`Linkloc2` varchar(50) DEFAULT NULL,`Linkloc3` varchar(50) DEFAULT NULL,`Linkalt1` varchar(50) DEFAULT NULL,`Linkalt2` varchar(50) DEFAULT NULL,`Linkalt3` varchar(50) DEFAULT NULL,`Counter` int(11) DEFAULT NULL,`Memo1` varchar(50) DEFAULT NULL,`Memo2` varchar(50) DEFAULT NULL,`Memo3` varchar(50) DEFAULT NULL,`Memo4` varchar(50) DEFAULT NULL,`Memo5` varchar(50) DEFAULT NULL,`TheDate` datetime DEFAULT NULL) CHARSET=utf8mb4;"
+            if j == 1:
+                print(str(i) + ":" + str(cell))
+                # 列印欄位標題
+                if i == sheet.max_column:
+                    autoCreateTable = input(
+                        "Would you like to auto create table? (y/n):")
+                    if(autoCreateTable == "y"):
+                        cursor = conn.cursor()
+                        cursor.execute(createTableSQLcommand)
+                        print("Table 'content' established")
+                        conn.commit()
+                        cursor.close()
 
-            if i >= 14 and i <= 53:
-                if str(cell) != "":
-                    MEETING_ATTACH_NAME = str(cell)
-                    MEETING_ATTACH_NAME += ","
-                    # print(MEETING_ATTACH_NAME)
-            if i >= 54 and i <= 93:
-                if i == 54:
-                    MEETING_DOC_NAME = str(cell)
-                if str(cell) != "":
-                    MEETING_ATTACH_LINK = str(cell)
-                    MEETING_ATTACH_LINK += ","
-                    # print(MEETING_ATTACH_LINK)
-            if i == 94:
-                print("94:" + str(cell))
+            insertSQLcommand = ""
+            if i == 1:
+                print("current process ID:" + str(cell))
+                insertSQLcommand += "INSERT INTO `content` VALUES(" + str(cell) + ","
+
+            # 已讀取完該列的所有欄位資料(最後一欄)
+            elif i == sheet.max_column:
+                insertSQLcommand += str(cell) + ")"
+                print("writing row data into DB...")
                 cursor = conn.cursor()
-                # cursor.execute("INSERT INTO meeting_records (MEETING_TITLE,MEETING_NUMBER,MEETING_DATE,MEETING_TIME,MEETING_VENUE,MEETING_CHARPERSON,MEETING_PARTICIPANT,MEETING_MINUTE,MEETING_TEXT,MEETING_DOC_NAME,MEETING_ATTACH_NAME) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);", (
-                #     MEETING_TITLE, MEETING_NUMBER, MEETING_DATE, MEETING_TIME, MEETING_VENUE, MEETING_CHARPERSON, MEETING_PARTICIPANT, MEETING_MINUTE, MEETING_TEXT, MEETING_DOC_NAME, MEETING_ATTACH_LINK))
+                cursor.execute(insertSQLcommand)
                 print("Inserted", cursor.rowcount, "row(s) of data.")
                 conn.commit()
                 print("Done.")
                 cursor.close()
 
-        conn.close()
-        print("Connection closed.")
+            # 其餘中間欄位資料
+            else:
+                insertSQLcommand += str(cell) + ","
+
+conn.close()
+print("Connection closed.")
