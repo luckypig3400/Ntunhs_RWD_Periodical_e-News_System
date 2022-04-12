baa8324f0db22878c54b0ab253e426aaf8e303a5
郵差 <twHenryYang@gmail.com>
2022-04-12 10:05:42 +0800

    feat: insert Excel each row data into DB

--- DB/excel2sqlDBconverter.py
+++ DB/excel2sqlDBconverter.py
@@ -1,3 +1,4 @@
+from types import coroutine
 import openpyxl
 import mysql.connector
 from mysql.connector import errorcode
@@ -16,18 +17,17 @@ try:
         user=config["user"],
         password=config["password"]
     )
+
     # https://www.w3schools.com/python/python_mysql_create_db.asp
-    # Ask user if he wish auto-create database
-    userInput = input("Would you like to auto create database? (y/n)")
-    if(userInput == "y"):
-        print(userInput)
-        cursor = conn.cursor()
-        cursor.execute(
-            "CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'utf8'".format(config["db"]))
-        print("Database '{}' established".format(config["db"]))
-        cursor.close()
-        conn.close()
-        conn = mysql.connector.connect(**config)
+    # Auto-create database
+    cursor = conn.cursor()
+    cursor.execute(
+        "CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'utf8'".format(config["db"]))
+    print("Database '{}' established".format(config["db"]))
+    cursor.close()
+    conn.close()
+    conn = mysql.connector.connect(**config)
+
     print("Connection established")
 
 except mysql.connector.Error as err:
@@ -44,41 +44,56 @@ else:
     sheet = workbook['Content']
     # 讀取指定的單元格資料
     for j in range(1, sheet.max_row + 1):
+        # loop for each row
+        # https://openpyxl.readthedocs.io/en/stable/api/openpyxl.worksheet.worksheet.html#openpyxl.worksheet.worksheet.Worksheet.max_row
+        insertSQLcommand = ""
+
         for i in range(1, sheet.max_column + 1):
+            # loop for each column
             cell = sheet.cell(row=j, column=i).value
             createTableSQLcommand = "CREATE TABLE IF NOT EXISTS `content` (`Serial` int(11) NOT NULL,`NoID` varchar(50) DEFAULT NULL,`NoYear` varchar(4) DEFAULT NULL,`NoMonth` varchar(2) DEFAULT NULL,`NoClass` varchar(50) DEFAULT NULL,`Subject` varchar(50) DEFAULT NULL,`Writer` varchar(50) DEFAULT NULL,`StarID` int(11) DEFAULT NULL,`Email` varchar(50) DEFAULT NULL,`Content1` mediumtext DEFAULT NULL,`Content2` mediumtext DEFAULT NULL,`Content3` mediumtext DEFAULT NULL,`Photo1` varchar(50) DEFAULT NULL,`Photo2` varchar(50) DEFAULT NULL,`Photo3` varchar(50) DEFAULT NULL,`Alt1` varchar(50) DEFAULT NULL,`Alt2` varchar(50) DEFAULT NULL,`Alt3` varchar(50) DEFAULT NULL,`Temp1` varchar(15) DEFAULT NULL,`Temp2` varchar(15) DEFAULT NULL,`Temp3` varchar(15) DEFAULT NULL,`TempColor` varchar(50) DEFAULT NULL,`Link1` mediumtext DEFAULT NULL,`Link2` mediumtext DEFAULT NULL,`Link3` mediumtext DEFAULT NULL,`Linkloc1` varchar(50) DEFAULT NULL,`Linkloc2` varchar(50) DEFAULT NULL,`Linkloc3` varchar(50) DEFAULT NULL,`Linkalt1` varchar(50) DEFAULT NULL,`Linkalt2` varchar(50) DEFAULT NULL,`Linkalt3` varchar(50) DEFAULT NULL,`Counter` int(11) DEFAULT NULL,`Memo1` varchar(50) DEFAULT NULL,`Memo2` varchar(50) DEFAULT NULL,`Memo3` varchar(50) DEFAULT NULL,`Memo4` varchar(50) DEFAULT NULL,`Memo5` varchar(50) DEFAULT NULL,`TheDate` datetime DEFAULT NULL) CHARSET=utf8mb4;"
+
+            # first row, create table
             if j == 1:
                 print(str(i) + ":" + str(cell))
                 # 列印欄位標題
                 if i == sheet.max_column:
-                    autoCreateTable = input(
-                        "Would you like to auto create table? (y/n):")
-                    if(autoCreateTable == "y"):
-                        cursor = conn.cursor()
-                        cursor.execute(createTableSQLcommand)
-                        print("Table 'content' established")
-                        conn.commit()
-                        cursor.close()
-
-            insertSQLcommand = ""
-            if i == 1:
-                print("current process ID:" + str(cell))
-                insertSQLcommand += "INSERT INTO `content` VALUES(" + str(cell) + ","
-
-            # 已讀取完該列的所有欄位資料(最後一欄)
-            elif i == sheet.max_column:
-                insertSQLcommand += str(cell) + ")"
-                print("writing row data into DB...")
-                cursor = conn.cursor()
-                cursor.execute(insertSQLcommand)
-                print("Inserted", cursor.rowcount, "row(s) of data.")
-                conn.commit()
-                print("Done.")
-                cursor.close()
-
-            # 其餘中間欄位資料
+                    cursor = conn.cursor()
+                    cursor.execute(createTableSQLcommand)
+                    print("Table 'content' established")
+                    conn.commit()
+                    cursor.close()
+            # other rows, insert data
             else:
-                insertSQLcommand += str(cell) + ","
+                if i == 1:
+                    print("current process ID:" + str(cell))
+                    insertSQLcommand += "INSERT INTO `content` (`Serial`, `NoID`, `NoYear`, `NoMonth`, `NoClass`, `Subject`, `Writer`, `StarID`, `Email`, `Content1`, `Content2`, `Content3`, `Photo1`, `Photo2`, `Photo3`, `Alt1`, `Alt2`, `Alt3`, `Temp1`, `Temp2`, `Temp3`, `TempColor`, `Link1`, `Link2`, `Link3`, `Linkloc1`, `Linkloc2`, `Linkloc3`, `Linkalt1`, `Linkalt2`, `Linkalt3`, `Counter`, `Memo1`, `Memo2`, `Memo3`, `Memo4`, `Memo5`, `TheDate`) VALUES(" + str(cell) + ","
+
+                # StarID(int)
+                elif i == 8:
+                    if str(cell) == "None":
+                        insertSQLcommand += "NULL,"
+                    else:
+                        insertSQLcommand += str(cell) + ","
+                # Counter(int)
+                elif i == 32:
+                    insertSQLcommand += str(cell) + ","
+                # 已讀取完該列的所有欄位資料(最後一欄)
+                elif i == sheet.max_column:
+                    insertSQLcommand += "'" + str(cell) + "')"
+                    cursor = conn.cursor()
+                    # print(insertSQLcommand)
+                    cursor.execute(insertSQLcommand)
+                    print("Inserted", cursor.rowcount, "row(s) of data.")
+                    conn.commit()
+                    print("Done.")
+                    cursor.close()
+                # 其餘中間欄位資料(用''包住代表字串)
+                else:
+                    if str(cell) == "None":
+                        insertSQLcommand += "NULL,"
+                    else:
+                        insertSQLcommand += "'" + str(cell) + "',"
 
 conn.close()
 print("Connection closed.")
