97576764ee5a66deaacaf30d7899fe3ac0827968
郵差 <twHenryYang@gmail.com>
2022-04-22 19:42:03 +0800

    style: create RWD search form

--- views/search.php
+++ views/search.php
@@ -32,27 +32,37 @@ require_once("partials/head.php");
     <section id="blog" class="blog">
       <!-- 搜尋欄區塊 -->
       <div class="container" data-aos="fade-up">
+
         <div class="sidebar col-lg-12 entries">
-          <h3 class="sidebar-title">搜尋</h3>
-          <div class="sidebar-item search-form">
-            <form action="">
-              <input type="text">
-              <button type="submit"><i class="bi bi-search"></i></button>
-            </form>
-          </div><!-- End sidebar search formn-->
 
-          <h3 class="sidebar-title">文章分類</h3>
-          <div class="sidebar-item categories">
-            <ul>
-              <li><a href="#">頭條新聞<span>(3)</span></a></li>
-              <li><a href="#">特別報導<span>(5)</span></a></li>
-              <li><a href="#">校園尚青<span>(5)</span></a></li>
-              <li><a href="#">心靈立可白<span>(2)</span></a></li>
-              <li><a href="#">業務報導<span>(1)</span></a></li>
-            </ul>
-          </div><!-- End sidebar categories-->
+          <!-- https://www.w3schools.com/howto/howto_css_stacked_form.asp -->
+          <form action="">
+            <h3 class="blog-title">文字搜尋</h3>
+            <input type="text" id="searchText" name="firstname" placeholder="Your name..">
+            <br><br>
+            <h3 class="blog-title">文章分類</h3>
+            <div class="sidebar-title categories tags">
+              <ul>
+                <li><a href="#">頭條新聞</a></li>
+                <li><a href="#">特別報導</a></li>
+                <li><a href="#">校園尚青</a></li>
+                <li><a href="#">心靈立可白</a></li>
+                <li><a href="#">業務報導</a></li>
+              </ul>
+            </div><!-- End sidebar categories-->
+            <br>
+            <h3 class="blog-title">選擇期別</h3>
+            <select id="country" name="country">
+              <option value="all">在所有期別內搜索</option>
+              <option value="219">第219期</option>
+              <option value="218">第218期</option>
+              <option value="217">第217期</option>
+            </select>
 
-          <h3 class="sidebar-title">選擇期別</h3>
+            <input type="submit" value="送出查詢">
+          </form>
+
+          <h3 class="blog-title">選擇期別</h3>
           <div class="sidebar-item tags">
             <ul>
               <li><a href="#">217</a></li>
@@ -65,7 +75,9 @@ require_once("partials/head.php");
           </div><!-- End sidebar tags-->
 
         </div><!-- 搜尋欄區塊 -->
+
       </div>
+
     </section><!-- End Blog Section -->
 
   </main><!-- End #main -->
