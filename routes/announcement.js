94553ee55b67c503b4261d18f19352643bb60607
楊嘉翔 <81738019+Yang-Jiaxiang@users.noreply.github.com>
2022-04-29 12:15:04 +0800

    announcement 增加欄位dateTime

--- routes/announcement.js
+++ routes/announcement.js
@@ -15,9 +15,10 @@ router
     })
     .post(async (req, res) => {
         try {
-            const { text } = req.body
-            const result = await ANNOUNCEMENT.create({ text })
-            return res.status(201).json({ id: result.insertId, text })
+            const { text,dateTime } = req.body
+            console.log(dateTime);
+            const result = await ANNOUNCEMENT.create({ text,dateTime })
+            return res.status(201).json({ id: result.insertId, text,dateTime })
         } catch (error) {
             return res.status(500).json({ error: error.message })
         }
