7556be2561574106b0e939e25078b5e306e6b13b
楊嘉翔 <81738019+Yang-Jiaxiang@users.noreply.github.com>
2022-05-29 13:50:34 +0800

    使用者管理介面修改

--- src/page/User.js
+++ src/page/User.js
@@ -1,17 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Modal, Box } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { getUser } from "../axios";
 import { deleteUser, editUser } from "../axios/putAxios";
-const config = require("../config/default.json");
-
-const apiURL = config.apiURL;
+import CreateUser from "./CreateUser";
 
 const User = () => {
     const [user, setUser] = useState([]);
-    const [createUserButton, setUserButton] = useState(true);
     const [userEditRender, setUserEditRender] = useState(false);
     const [userContent, setUserContent] = useState({});
     useEffect(async () => {
@@ -31,10 +26,10 @@ const User = () => {
         },
     ];
 
-    const userEditDiv = () => {
+    const UserEditDiv = () => {
         return (
             <div style={{ marginTop: "20px" }}>
-                姓名：
+                <h3 style={{ paddingBottom: "10px" }}>姓名：</h3>
                 <TextField
                     id="outlined-basic"
                     value={userContent.name}
@@ -45,9 +40,9 @@ const User = () => {
                             name: event.target.value,
                         });
                     }}
-                    sx={{ marginRight: "20px" }}
+                    style={{ paddingBottom: "20px", width: "100%" }}
                 />
-                帳號：
+                <h3 style={{ paddingBottom: "10px" }}>帳號：</h3>
                 <TextField
                     id="outlined-basic"
                     value={userContent.username}
@@ -58,9 +53,9 @@ const User = () => {
                             username: event.target.value,
                         });
                     }}
-                    sx={{ marginRight: "20px" }}
+                    style={{ paddingBottom: "20px", width: "100%" }}
                 />
-                新密碼：
+                <h3 style={{ paddingBottom: "10px" }}>新密碼：</h3>
                 <TextField
                     id="outlined-basic"
                     value={userContent.password}
@@ -72,14 +67,14 @@ const User = () => {
                         });
                         console.log(userContent);
                     }}
-                    sx={{ marginRight: "20px" }}
+                    style={{ paddingBottom: "20px", width: "100%" }}
                 />
                 <div style={{ marginTop: "20px" }}>
                     <Button
                         variant="contained"
                         sx={{
                             height: "55px",
-                            width: "100px",
+                            width: "100%",
                             marginRight: "20px",
                         }}
                         onClick={() => {
@@ -91,9 +86,13 @@ const User = () => {
                         修改
                     </Button>
                     <Button
-                        variant="outlined"
+                        variant="contained"
                         color="error"
-                        sx={{ height: "55px", width: "auto" }}
+                        sx={{
+                            height: "55px",
+                            width: "100%",
+                            marginTop: "20px",
+                        }}
                         onClick={() => {
                             var message = window.confirm(
                                 `確定要刪除${userContent.name}?`
@@ -116,6 +115,27 @@ const User = () => {
         );
     };
 
+    const handleClick = () => {
+        setUserEditRender(true);
+    };
+    const handleClose = () => {
+        setUserEditRender(false);
+    };
+
+    const style = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 400,
+        bgcolor: "background.paper",
+        borderRadius: "10px",
+        boxShadow: 24,
+        pt: 2,
+        px: 4,
+        pb: 3,
+    };
+
     return (
         <>
             <div className="headerTitle">成員管理</div>
@@ -132,23 +152,98 @@ const User = () => {
                                     username: user.row.username,
                                     name: user.row.name,
                                 });
-                                setUserEditRender(true);
+                                handleClick();
                                 setUserButton(false);
                             }}
                         />
                     </div>
                 </div>
                 <div style={{ paddingTop: "20px" }}></div>
-                {createUserButton ? (
-                    <Link to="/CreateUser">
-                        <Button variant="contained">註冊新使用者</Button>
-                    </Link>
-                ) : (
-                    ""
-                )}
-                {userEditRender ? userEditDiv() : ""}
+                <CreateUserBox />
+
+                <Modal
+                    open={userEditRender}
+                    onClose={handleClose}
+                    aria-labelledby="parent-modal-title"
+                    aria-describedby="parent-modal-description"
+                >
+                    <Box sx={{ ...style, width: 400 }}>
+                        <UserEditDiv />
+
+                        <Button
+                            variant="outlined"
+                            color="error"
+                            sx={{
+                                width: "100%",
+                                height: "50px",
+                                marginTop: "20px",
+                            }}
+                            onClick={() => {
+                                handleClose();
+                            }}
+                        >
+                            取消
+                        </Button>
+                    </Box>
+                </Modal>
             </div>
         </>
     );
 };
+
+const CreateUserBox = () => {
+    const [createOpen, setcreateOpen] = React.useState(false);
+    const handleOpen = () => {
+        setcreateOpen(true);
+    };
+    const handleClose = () => {
+        setcreateOpen(false);
+    };
+
+    const style = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 400,
+        bgcolor: "background.paper",
+        borderRadius: "10px",
+        boxShadow: 24,
+        pt: 2,
+        px: 4,
+        pb: 3,
+    };
+    return (
+        <div>
+            <Button onClick={handleOpen} variant="contained">
+                註冊新使用者
+            </Button>
+            <Modal
+                open={createOpen}
+                onClose={handleClose}
+                aria-labelledby="parent-modal-title"
+                aria-describedby="parent-modal-description"
+            >
+                <Box sx={{ ...style, width: 400 }}>
+                    <CreateUser />
+                    <Button
+                        variant="outlined"
+                        color="error"
+                        sx={{
+                            width: "100%",
+                            height: "50px",
+                            marginTop: "20px",
+                        }}
+                        onClick={() => {
+                            handleClose();
+                        }}
+                    >
+                        取消
+                    </Button>
+                </Box>
+            </Modal>
+        </div>
+    );
+};
+
 export default User;
