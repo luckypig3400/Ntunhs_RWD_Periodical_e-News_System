af3b5ca14c13e0a24e8910359aad8fc96cf7b135
郵差 <twHenryYang@gmail.com>
2022-06-19 14:12:50 +0800

    fix: remove all HTML tags carefully

--- controller/simplifyArticleContent.php
+++ controller/simplifyArticleContent.php
@@ -44,8 +44,13 @@ function simplifyArticleContent($in_articleContent, $max_length)
     // After explode with <br>, remove the redundant <br> tags
     $p = str_replace("<br>", "", $p);
     $p = str_replace("<br/>", "", $p);
-    // remove all HTML tag
-    preg_match_all("/<.*>/", $simplifiedContent, $matches);
+    
+    // remove all HTML tags
+    preg_match_all("/<.*>/", $p, $matches);
+    foreach ($matches as $match) {
+      $p = str_replace($match, "", $p);
+    }
+    // remove all HTML tags finished !
 
     if (strlen($p) + strlen($simplifiedContent) > $max_length) {
       // https://stackoverflow.com/questions/10934711/truncating-chinese-text
