82df177f6a40e2c6ec6806784cc790837b15128f
郵差 <twHenryYang@gmail.com>
2022-06-22 11:01:28 +0800

    update: check if periodical table cover images exists

--- DB/checkImagesExist.php
+++ DB/checkImagesExist.php
@@ -7,6 +7,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+	echo "<h2>舊版content資料表檢查</h2>";
     $sqlCommand = "SELECT `Serial`,`Photo1`,`Photo2`,`Photo3` FROM content";
 
     $stmt = $conn->prepare($sqlCommand);
@@ -38,6 +39,28 @@ try {
             }
         }
     }
+
+	//======================================================================
+	echo "<h2>新版periodical資料表檢查</h2>";
+	$newSqlCommand = "SELECT `id`,`cover` FROM periodical";
+
+    $stmt = $conn->prepare($newSqlCommand);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC); // set the resulting array to associative
+
+    $result = $stmt->fetchAll();
+
+    foreach ($result as $row) {
+        $id = $row['id'];
+        $coverImage = $row['cover'];
+
+        if ($coverImage != null) {
+            if (!file_exists("../public/image/$coverImage")) {
+                echo "<b>ID:$id Error:</b> $coverImage does not exist in data folder<br>";
+            }
+        }
+    }
+	
 } catch (PDOException $e) {
     echo "<b>Error Occured while asseccing MySQL DB:</b>" . $e->getMessage() . "<br>";
 }
@@ -50,7 +73,7 @@ try {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>舊伺服器檔案檢查器</title>
+    <title>期刊圖片檔案檢查器</title>
 </head>
 
 <body>
