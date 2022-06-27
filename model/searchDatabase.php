1f057cd0af07b711e7c4a98e983258ef40250e11
郵差 <twHenryYang@gmail.com>
2022-06-27 16:07:06 +0800

    fix: search SQL syntax error with multi conditions

--- model/searchDatabase.php
+++ model/searchDatabase.php
@@ -12,21 +12,21 @@ function searchDatabase($searchKeyword, $searchCategory, $searchPeriod)
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $sql = "SELECT * FROM `periodical` WHERE (`quillcontent` LIKE '%" . $searchKeyword . "%' OR `subject` LIKE '%" . $searchKeyword . "%') ORDER BY `periodical`.`id` DESC";
-        // echo $sql;
+        $sql = "SELECT * FROM `periodical` WHERE (`quillcontent` LIKE '%" . $searchKeyword . "%' OR `subject` LIKE '%" . $searchKeyword . "%')";
         if ($searchCategory != "all" && $searchCategory != "") {
             $sql .= " AND `categoryID` = '" . $searchCategory . "'";
         }
         if ($searchPeriod != "all" && $searchPeriod != "") {
             $sql .= " AND `periodNumber` = '" . $searchPeriod . "'";
         }
+        $sql .= " ORDER BY `periodical`.`id` DESC";
 
         $stmt = $conn->prepare($sql);
         $stmt->execute();
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
         $result = $stmt->fetchAll();
-
+        // echo "<h1>$sql</h1>";
         return $result;
     } catch (PDOException $e) {
         echo "Something went wrong while accessing MySQL DB, ERROR:" . $e;
